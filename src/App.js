a5c63f166ea34d0c464cb52749cd5d224a13963d
Shreyansh Shrey <shreyansh.shrey1999@gmail.com>
2020-10-13 16:18:09 +0530

    InfoBox Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
+import InfoBox from "./InfoBox";
 import './App.css';
 
 function App() {
 
-  const [countries, setContries] = useState([
-    "USA", "UK", "INDIA"
-  ]);
+  const [countries, setContries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
 
   useEffect(() => {
     const getCountriesData = async() => {
@@ -25,12 +25,21 @@ function App() {
     getCountriesData();
   }, []);
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select 
+          variant="outlined"
+          onChange={onCountryChange}
+          value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {
               countries.map(country=>
               <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -38,6 +47,23 @@ function App() {
             }
           </Select>
         </FormControl>
+      </div>
+      <div className="app__stats">
+          <InfoBox 
+          title="Coronavirus Cases"
+          cases="dvc"
+          total="sdff"
+          />
+          <InfoBox 
+          title="Recovered"
+          cases="dvc"
+          total="sdff"
+          />
+          <InfoBox 
+          title="Deaths"
+          cases="dvc"
+          total="sdff"
+          />
       </div>
     </div>
   );
